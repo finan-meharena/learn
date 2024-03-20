0989f062242a0d38eb44b7be110d824cbaa07fe7
Finan Meharena <finanmeharenakidane@gmail.com>
2024-03-20 14:02:58 -0700

    created store, reducer and action types

--- redux/mosh/03-redux-fundamentals/redux-starter/src/index.js
+++ redux/mosh/03-redux-fundamentals/redux-starter/src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+import store from './store';
+import { bugAdded, bugRemoved } from './actionCreator';
+
+
+const unsubscribe = store.subscribe(() => {         // subscribe method returns a function that can be used to unsubscribe the listener.
+    console.log("Store changed!", store.getState());
+})
+
+store.dispatch(bugAdded("Bug 1"));
+
+unsubscribe(); // unsubscribing the listener, no notification will be received after this line.
+
+store.dispatch(bugRemoved(1));
+console.log(store.getState()); // initialy state is empty array coz we have dispatched no action yet.
+
+
