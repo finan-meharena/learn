d8de84953d90f6f52b3321c4eb579fccfe1ec99e
Finan Meharena <finanmeharenakidane@gmail.com>
2024-03-20 20:53:50 -0700

    created custome store

--- redux/mosh/04-redux-from-scratch/redux-starter/src/index.js
+++ redux/mosh/04-redux-from-scratch/redux-starter/src/index.js
@@ -1,20 +1,6 @@
-import store from './store';
-import { bugAdded, bugRemoved, bugResolved } from './actionCreator';
+import store from './customStore'
+import * as actions from './actionCreator'
 
 
-const unsubscribe = store.subscribe(() => {         // subscribe method returns a function that can be used to unsubscribe the listener.
-    console.log("Store changed!", store.getState());
-})
-
-store.dispatch(bugAdded("Bug 1"));
-store.dispatch(bugAdded("Bug 2"));
-store.dispatch(bugAdded("Bug 3"));
-store.dispatch(bugResolved(1))
-store.dispatch(bugResolved(2))
-
-unsubscribe(); // unsubscribing the listener, no notification will be received after this line.
-
-store.dispatch(bugRemoved(1));
-console.log(store.getState()); // initialy state is empty array coz we have dispatched no action yet.
-
-
+store.dispatch(actions.bugAdded("Bug 1"))
+console.log(store.getState())
