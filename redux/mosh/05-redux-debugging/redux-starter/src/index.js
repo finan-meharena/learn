7d2e12b33f35adcd768661072786500b99f6a135
Finan Meharena <finanmeharenakidane@gmail.com>
2024-03-21 20:12:09 -0700

    worked around redux devtools

--- redux/mosh/05-redux-debugging/redux-starter/src/index.js
+++ redux/mosh/05-redux-debugging/redux-starter/src/index.js
@@ -1,6 +1,8 @@
-import store from './customStore'
+import store from './store' // import from store, not customStore
 import * as actions from './actionCreator'
 
-
 store.dispatch(actions.bugAdded("Bug 1"))
+store.dispatch(actions.bugAdded("Bug 2"))
+store.dispatch(actions.bugAdded("Bug 3"))
+store.dispatch(actions.bugResolved(1));
 console.log(store.getState())
--- redux/mosh/05-redux-debugging/redux-starter/src/store.js
+++ redux/mosh/05-redux-debugging/redux-starter/src/store.js
@@ -1,6 +1,11 @@
 import {createStore} from 'redux';
+import {devToolsEnhancer} from 'redux-devtools-extension' // enhancer 
 import reducer from './reducer';
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    devToolsEnhancer({trace: true})
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // allows our store to talk to dev tools
+    );
 
 export default store;
