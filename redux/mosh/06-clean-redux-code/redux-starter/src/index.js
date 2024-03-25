76ca2b8d266c3ccc4d3ef9e025e5ecea06d0045f
Finan Meharena <finanmeharenakidane@gmail.com>
2024-03-25 12:39:18 -0700

    renamed store, fixed import issue in index

--- redux/mosh/06-clean-redux-code/redux-starter/src/index.js
+++ redux/mosh/06-clean-redux-code/redux-starter/src/index.js
@@ -1,5 +1,7 @@
-import store from './store/store' // import from store, not customStore
+import configureStore from './store/configureStore' // import from store, not customStore
 import {bugAdded, bugRemoved, bugResolved} from './store/bugs'
+
+const store = configureStore();
 
 store.dispatch(bugAdded("Bug 1"))
 store.dispatch(bugAdded("Bug 2"))
