ac87ad447937ece6d20df9fd5b1751b9bc6629f8
Finan Meharena <finanmeharenakidane@gmail.com>
2024-03-26 13:34:57 -0700

    added slice using redux toolkit to bugs

--- redux/mosh/06-clean-redux-code/redux-starter/src/store/bugs.js
+++ redux/mosh/06-clean-redux-code/redux-starter/src/store/bugs.js
@@ -1,51 +1,26 @@
-import {createAction, createReducer} from "@reduxjs/toolkit"
-
-const action = createAction("bugUpdated")
-// console.log(action({id : 1}));
-// console.log(action.type)
-
-// Action types --> we dont need those constants coz we can get them from the creationAction result's type property
-// const BUG_ADDED = 'bugAdded';
-// const BUG_REMOVED = 'bugRemoved';
-// const BUG_RESOLVED = 'bugResolved';
-
-// Actions (use cmd + d to enable multicursor editting)
-export const bugAdded = createAction("bugAdded")
-
-export const bugResolved = createAction("bugResolved")
-
-export const bugRemoved = createAction("bugRemoved");
+import {createSlice} from "@reduxjs/toolkit"
 
 // Reducer
 let lastId = 0;
 
-export default createReducer([], {
-    // key : value pairs (key = actions, value = functions that handle those actions)
-    [bugAdded.type]: (bugs, action) => { // this is mutating code, redux uses immer to apply changes
-        bugs.push({
-            id: ++lastId,
-            description: action.payload.description,
-            resolved: false
-        })
-    },
-    // bugAdded: (bugs, action) => { // this is mutating code, redux uses immer to apply changes
-    //     bugs.push({
-    //         id: ++lastId,
-    //         description: action.payload.description,
-    //         resolved: false
-    //     })
-    // },
-    // dynamically compute action name
-    [bugResolved.type]: (bugs, action) =>{
-        const index = bugs.findIndex(bug => bug.id === action.payload.id)
-        bugs[index].resolved = true;
-    },
-    // bugResolved: (bugs, action) =>{
-    //     const index = bugs.findIndex(bug => bug.id === action.payload.id)
-    //     bugs[index].resolved = true;
-    // },
-
+const slice = createSlice({
+    name: 'bugs',
+    initialState: [],
+    reducers: {
+        bugAdded : (bugs, action) => {
+            bugs.push({
+                id: ++lastId,
+                description: action.payload.description,
+                resolved: false
+            })
+        }, 
+        bugResolved : (bugs, action) => {
+            const index = bugs.findIndex(bug => bug.id === action.payload.id)
+            bugs[index].resolved = true;
+        }
+    }
 })
-
+export const {bugAdded, bugResolved} = slice.actions
+export default slice.reducer;
 
 
