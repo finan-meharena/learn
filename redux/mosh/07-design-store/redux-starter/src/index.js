1200459be49806c9e4b9e23498828a3a619b0ef2
Finan Meharena <finanmeharenakidane@gmail.com>
2024-03-26 17:28:41 -0700

    memoizaed getUnresolvedBugs using reselect

--- redux/mosh/07-design-store/redux-starter/src/index.js
+++ redux/mosh/07-design-store/redux-starter/src/index.js
@@ -1,13 +1,19 @@
-import { bugAdded, bugResolved } from './store/bugs';
+import { bugAdded, bugResolved , getUnresolvedBugs} from './store/bugs';
 import configureStore from './store/configureStore' // import from store, not customStore
 import {projectAdded} from './store/projects'
 
+
 const store = configureStore();
 
-store.dispatch(projectAdded({name : "Project 1"})) // pass actions as objects
-store.dispatch(projectAdded({name : "Project 2"}))
-store.dispatch(bugAdded({description : "Bug 1"}))
-store.dispatch(bugResolved({id : 1}))
+store.dispatch(projectAdded({ name: "Project 1" }));
+store.dispatch(projectAdded({ name: "Project 2" }));
+store.dispatch(projectAdded({ name: "Project 3" }));
+store.dispatch(bugAdded({ description: "Bug 1" }));
+store.dispatch(bugAdded({ description: "Bug 2" }));
+store.dispatch(bugAdded({ description: "Bug 3" }));
+store.dispatch(bugResolved({ id: 1 }));
 
+const x = getUnresolvedBugs(store.getState());
+const y = getUnresolvedBugs(store.getState()); 
 
-console.log(store.getState())
+console.log(x === y); // true becase the selector is memoized and the result is cached
--- redux/mosh/07-design-store/redux-starter/src/store/bugs.js
+++ redux/mosh/07-design-store/redux-starter/src/store/bugs.js
@@ -1,5 +1,5 @@
 import {createSlice} from "@reduxjs/toolkit"
-
+import {createSelector} from 'reselect'
 // Reducer
 let lastId = 0;
 
@@ -22,5 +22,15 @@ const slice = createSlice({
 })
 export const {bugAdded, bugResolved} = slice.actions
 export default slice.reducer;
+
+// selectors
+// export const getUnreslovedBugs = state => state.entities.bugs.filter(bug => !bug.resolved)
+
+// Memoization (memoiz the selector to increase preformance)
+export const getUnresolvedBugs = createSelector( // takes a list of input selectors and a 
+                                                //function that calculates the value based on the input selectors
+    state => state.entities.bugs, //
+    bugs => bugs.filter(bug => !bug.resolved)
+)
 
 
