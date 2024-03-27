827092d2853b227ba300632930b68d54e5fc3110
Finan Meharena <finanmeharenakidane@gmail.com>
2024-03-27 14:39:25 -0700

    reviewd logger

--- redux/mosh/08-middleware/redux-starter/src/index.js
+++ redux/mosh/08-middleware/redux-starter/src/index.js
@@ -7,7 +7,7 @@ import {userAdded } from './store/users';
 const store = configureStore();
 
 store.dispatch(userAdded({name : "finan"}))
-// store.dispatch(userAdded({name : "kidane"}))
+store.dispatch(userAdded({name : "kidane"}))
 // store.dispatch(projectAdded({ name: "Project 1" }));
 // store.dispatch(projectAdded({ name: "Project 2" }));
 // store.dispatch(projectAdded({ name: "Project 3" }));
@@ -18,4 +18,10 @@ store.dispatch(userAdded({name : "finan"}))
 // store.dispatch(assingBugToUser({bugId : 2, userId : 1}))
 // store.dispatch(bugResolved({ id: 1 }));
 
+const x = getUnresolvedBugs(store.getState());
+const y = getUnresolvedBugs(store.getState()); 
 
+//getBugsByUser(1) returns a function. that function takes a state and returns list of bugs assinged to a user
+const bugs = getBugsByUser(1)(store.getState());
+console.log(bugs)
+console.log(x === y); // true becase the selector is memoized and the result is cached
--- redux/mosh/08-middleware/redux-starter/src/store/middleware/loggger.js
+++ redux/mosh/08-middleware/redux-starter/src/store/middleware/loggger.js
@@ -1,7 +1,12 @@
-const logger = store => next => action => {
+// next is reference to the next middleware function 
+// (if onl one middleware fucntion next is the root reducer)
+
+// SNA, like DNA (Store, Next, Action)
+const logger = store => next => action => { 
     console.log("store : ", store);
     console.log("next : ", next);
     console.log("action : ", action);
+    next(action); // calling the next middleware function (rooter reducer, in our case) so our state gonna be updated.
 }
 
 export default logger;
