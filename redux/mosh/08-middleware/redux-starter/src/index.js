37a8af688433bb4e8bb9091e4963f064c98b846c
Finan Meharena <finanmeharenakidane@gmail.com>
2024-04-01 10:37:39 -0700

    added middleware function

--- redux/mosh/08-middleware/redux-starter/src/index.js
+++ redux/mosh/08-middleware/redux-starter/src/index.js
@@ -5,23 +5,30 @@ import {userAdded } from './store/users';
 
 
 const store = configureStore();
+// store.dispatch({}); // action should have plain object with type property
+// store.dispatch( () => {}) // action shouldbe plain oject with type propety
 
-store.dispatch(userAdded({name : "finan"}))
-store.dispatch(userAdded({name : "kidane"}))
-// store.dispatch(projectAdded({ name: "Project 1" }));
-// store.dispatch(projectAdded({ name: "Project 2" }));
-// store.dispatch(projectAdded({ name: "Project 3" }));
-// store.dispatch(bugAdded({ description: "Bug 1" }));
-// store.dispatch(bugAdded({ description: "Bug 2" }));
-// store.dispatch(bugAdded({ description: "Bug 3" }));
-// store.dispatch(assingBugToUser({bugId : 1, userId : 1}))
-// store.dispatch(assingBugToUser({bugId : 2, userId : 1}))
-// store.dispatch(bugResolved({ id: 1 }));
+store.dispatch((dispatch, getState) => {
+    dispatch({type : 'bugsReceived', bugs : [1, 2, 3]})
+    console.log(getState())
+})
 
-const x = getUnresolvedBugs(store.getState());
-const y = getUnresolvedBugs(store.getState()); 
+// store.dispatch(userAdded({name : "finan"}))
+// store.dispatch(userAdded({name : "kidane"}))
+// // store.dispatch(projectAdded({ name: "Project 1" }));
+// // store.dispatch(projectAdded({ name: "Project 2" }));
+// // store.dispatch(projectAdded({ name: "Project 3" }));
+// // store.dispatch(bugAdded({ description: "Bug 1" }));
+// // store.dispatch(bugAdded({ description: "Bug 2" }));
+// // store.dispatch(bugAdded({ description: "Bug 3" }));
+// // store.dispatch(assingBugToUser({bugId : 1, userId : 1}))
+// // store.dispatch(assingBugToUser({bugId : 2, userId : 1}))
+// // store.dispatch(bugResolved({ id: 1 }));
 
-//getBugsByUser(1) returns a function. that function takes a state and returns list of bugs assinged to a user
-const bugs = getBugsByUser(1)(store.getState());
-console.log(bugs)
-console.log(x === y); // true becase the selector is memoized and the result is cached
+// const x = getUnresolvedBugs(store.getState());
+// const y = getUnresolvedBugs(store.getState()); 
+
+// //getBugsByUser(1) returns a function. that function takes a state and returns list of bugs assinged to a user
+// const bugs = getBugsByUser(1)(store.getState());
+// console.log(bugs)
+// console.log(x === y); // true becase the selector is memoized and the result is cached
--- redux/mosh/08-middleware/redux-starter/src/store/configureStore.js
+++ redux/mosh/08-middleware/redux-starter/src/store/configureStore.js
@@ -1,11 +1,16 @@
 import {configureStore} from '@reduxjs/toolkit'
 import reducer from './reducer';
 import logger from './middleware/loggger';
+import func from './middleware/func';
+
 
 export default function(){
     return configureStore({
         reducer,
-        middleware: [logger({destination : "Console"})],
+        middleware: [
+            logger({destination : "Console"}),
+            func
+        ],
         // middleware: [logger( "Console")]
     });  
 }
