0615c5cef524e69a95cc3a62fa7f895e93cfbac9
Finan Meharena <finanmeharenakidane@gmail.com>
2024-04-01 10:41:36 -0700

    added redux-thunk to project

--- redux/mosh/08-middleware/redux-starter/src/store/configureStore.js
+++ redux/mosh/08-middleware/redux-starter/src/store/configureStore.js
@@ -1,15 +1,14 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit'
 import reducer from './reducer';
 import logger from './middleware/loggger';
-import func from './middleware/func';
 
 
 export default function(){
     return configureStore({
         reducer,
         middleware: [
+            ...getDefaultMiddleware(),
             logger({destination : "Console"}),
-            func
         ],
         // middleware: [logger( "Console")]
     });  
