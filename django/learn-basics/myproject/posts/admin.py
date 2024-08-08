9f665512ee54144bdfd13eedec2f42736b3da815
Finan Meharena <finanmeharenakidane@gmail.com>
2024-08-08 08:55:45 -0700

    created superuser, rendered posts and updated css

--- django/learn-basics/myproject/posts/admin.py
+++ django/learn-basics/myproject/posts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# Register your models here before u can see them on the admin page
+admin.site.register(Post)
