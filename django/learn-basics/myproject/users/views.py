d9a3308175c1e7dbbfda1485eff32109683be87c
Finan Meharena <finanmeharenakidane@gmail.com>
2024-08-11 22:42:25 -0700

    added form to reigster users

--- django/learn-basics/myproject/users/views.py
+++ django/learn-basics/myproject/users/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 
@@ -8,4 +10,13 @@ def users_home(request):
 
 
 def register(request):
-    return render(request, 'users/register.html')
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("posts:list")
+    else:
+        form = UserCreationForm()
+        
+    return render(request, 'users/register.html', {"form" : form})
