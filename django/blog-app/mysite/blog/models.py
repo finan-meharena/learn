bca7797552d398c70268d54828cf99729d8ad42c
Finan Meharena <finanmeharenakidane@gmail.com>
2025-01-03 12:52:56 -0800

    created custom manager, added views and templates for posts

--- django/blog-app/mysite/blog/models.py
+++ django/blog-app/mysite/blog/models.py
@@ -2,11 +2,17 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+    
+
 class Post(models.Model):
     
     class Status(models.TextChoices):
         DRAFT = "DF", "Draft"
-        PUBLISHED = "PB", "Published"
+        PUBLISHED = "PU", "Published"
         
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
@@ -16,6 +22,8 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
+    objects = models.Manager()
+    published = PublishedManager()
     
     class Meta:
         ordering = ['-publish']
