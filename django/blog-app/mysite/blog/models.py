63907cd0e8801adc0bb491473cdbf3afc45d1d7f
Finan Meharena <finanmeharenakidane@gmail.com>
2025-01-04 23:50:25 -0800

    added cannonical urls for post details

--- django/blog-app/mysite/blog/models.py
+++ django/blog-app/mysite/blog/models.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
+
 
 
 class PublishedManager(models.Manager):
@@ -31,3 +33,7 @@ class Post(models.Model):
         
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", kwargs={"id": self.id})
+    
--- django/blog-app/mysite/blog/urls.py
+++ django/blog-app/mysite/blog/urls.py
@@ -5,5 +5,4 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('<int:id>/', views.post_detail, name='post_detail'),
-    
 ]
