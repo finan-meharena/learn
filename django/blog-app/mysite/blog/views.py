9f980ed4d698eba871e55395b28adb06c5b18cbe
Finan Meharena <finanmeharenakidane@gmail.com>
2025-01-05 00:27:48 -0800

    added SEO friendly url for post_detail

--- django/blog-app/mysite/blog/views.py
+++ django/blog-app/mysite/blog/views.py
@@ -8,10 +8,14 @@ def post_list(request):
     
     return render(request, 'blog/post/list.html', {'posts': posts})
 
-
-def post_detail(request, id):
+def post_detail(request, year, month, day, slug):
     
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED, 
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day,
+                             slug=slug)
     
     return render(request, 'blog/post/detail.html', {'post': post})
     
