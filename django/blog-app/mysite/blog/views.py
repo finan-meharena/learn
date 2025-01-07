69746cc68ff54dd349f809699cf5d3fd0c65921c
Finan Meharena <finanmeharenakidane@gmail.com>
2025-01-07 10:39:40 -0800

    introduced class based views

--- django/blog-app/mysite/blog/views.py
+++ django/blog-app/mysite/blog/views.py
@@ -1,23 +1,23 @@
+from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
-# Create your views here.
 
-def post_list(request):
+# def post_list(request):
     
-    # Pagination with 3 posts per page
-    post_list = Post.published.all()
-    paginator = Paginator(object_list=post_list, per_page=3)
-    page_number = request.GET.get('page', 1)
+#     # Pagination with 3 posts per page
+#     post_list = Post.published.all()
+#     paginator = Paginator(object_list=post_list, per_page=3)
+#     page_number = request.GET.get('page', 1)
     
-    try:
-        posts = paginator.page(page_number)
-    except (EmptyPage , PageNotAnInteger):
-        # If page_number is out of range get last page of result
-        posts = paginator.page(paginator.num_pages)
+#     try:
+#         posts = paginator.page(page_number)
+#     except (EmptyPage , PageNotAnInteger):
+#         # If page_number is out of range get last page of result
+#         posts = paginator.page(paginator.num_pages)
     
-    return render(request, 'blog/post/list.html', {'posts': posts})
+#     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, slug):
     
@@ -31,8 +31,11 @@ def post_detail(request, year, month, day, slug):
     return render(request, 'blog/post/detail.html', {'post': post})
     
     
-    # try:
-    #     post = Post.published.get(id=id)
-    # except Post.DoesNotExist:
-    #     raise Http404("NO Post Found")
-    # return render(request, 'blog/post/detail.html', {'post': post})
+# class based views
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+    
