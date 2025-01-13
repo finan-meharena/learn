315c4cbff46208fd656f0b5caee6c283eb2a1917
Finan Meharena <finanmeharenakidane@gmail.com>
2025-01-12 21:39:27 -0800

    added model for comments

--- django/blog-app/mysite/blog/models.py
+++ django/blog-app/mysite/blog/models.py
@@ -42,3 +42,24 @@ class Post(models.Model):
                 ])  # we u visit blog:post_detail the urls is gonna be dynamically constructed using these args
                         # and its gonna look 'blog/year/month/day/slug (blog/2025/1/3/who-was-django-reinhardt)
     
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created'])
+        ]
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post}"
