aa511de2b5d7f161e538c07c156b35ce613ad2ec
Finan Meharena <finanmeharenakidane@gmail.com>
2025-01-23 10:18:05 -0800

    retrieved posts by similarity using taggit

--- django/blog-app/mysite/blog/views.py
+++ django/blog-app/mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
@@ -6,22 +7,32 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from .models import Post
 from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
 
-
-# def post_list(request):
+def post_list(request, tag_slug=None):
+    # Pagination with 3 posts per page
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list=post_list, per_page=3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except (EmptyPage , PageNotAnInteger):
+        # If page_number is out of range get last page of result
+        posts = paginator.page(paginator.num_pages)
     
-#     # Pagination with 3 posts per page
-#     post_list = Post.published.all()
-#     paginator = Paginator(object_list=post_list, per_page=3)
-#     page_number = request.GET.get('page', 1)
     
-#     try:
-#         posts = paginator.page(page_number)
-#     except (EmptyPage , PageNotAnInteger):
-#         # If page_number is out of range get last page of result
-#         posts = paginator.page(paginator.num_pages)
-    
-#     return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(
+        request, 
+        'blog/post/list.html', 
+        {
+            'posts': posts,
+            'tag': tag
+        }
+        )
 
 def post_detail(request, year, month, day, slug):
     
@@ -35,6 +46,14 @@ def post_detail(request, year, month, day, slug):
     comments = post.comments.filter(active=True)
     # Form for users to comment
     form = CommentForm()
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
     
     return render(
         request, 
@@ -43,6 +62,7 @@ def post_detail(request, year, month, day, slug):
             'post': post,
             'form': form,
             'comments': comments,
+            'similar_posts': similar_posts,
         })
 
 def post_share(request, post_id):
@@ -109,9 +129,9 @@ def post_comment(request, post_id):
 
 # class based views
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
     
